ecf8c9674eb1a6556be7ff0462e015095e423e32
mojtaba <mohammadsgrdev@gmail.com>
2023-01-14 13:16:02 +0330

    [UPDATE] : add pages and update component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useMap } from 'react-map-gl';
-import './App.css';
 import MainMap from './components/main-map/main-map';
 // import MapBoxMap from './components/main-map/mapbox-gl';
 import { HomeBottomBox, HomeBottomContainer, HomeBottomSearchBar, HomeTopBox, HomeTopContainer } from './app-style';
@@ -35,7 +34,12 @@ function App() {
       <HomeBottomContainer>
         <HomeBottomBox>
           <HomeBottomSearchBar>
-
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+              </svg>
+            </span>
+            <div>text</div>
           </HomeBottomSearchBar>
         </HomeBottomBox>
       </HomeBottomContainer>
--- src/app-style.jsx
+++ src/app-style.jsx
@@ -63,7 +63,7 @@ export const HomeBottomBox = styled.div`
     }
 `;
 
-export const HomeBottomSearchBar = ({ text }) => {
+export const HomeBottomSearchBar = ({ children }) => {
 
     const Box = styled.div`
         width: 100%;
@@ -87,12 +87,7 @@ export const HomeBottomSearchBar = ({ text }) => {
 
     return (
         <Box>
-            <span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                </svg>
-            </span>
-            <div>text</div>
+            {children}
         </Box>
     )
 
