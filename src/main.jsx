5ef8c8dd97228c12a88a534814cd9db43bb84edf
mohammad asghari <mohammadsgr8@gmail.com>
2023-01-15 21:29:37 -0800

    add app config

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,32 @@
 
+import { createEmotionCache, MantineProvider } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MapProvider } from 'react-map-gl';
 import { Provider } from 'react-redux';
+import rtlPlugin from 'stylis-plugin-rtl';
 import App from './App';
 import './index.css';
 import { store } from './store/store';
+
+const rtlCache = createEmotionCache({
+  key: 'mantine-rtl',
+  stylisPlugins: [rtlPlugin],
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store} >
-      <MapProvider>
-        <App />
-      </MapProvider>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        emotionCache={rtlCache}
+        theme={{ dir: 'rtl' }}
+      >
+        <MapProvider>
+          <App />
+        </MapProvider>
+      </MantineProvider>
     </Provider>
   </React.StrictMode>,
 )
