b53c9ed9819994d0999051b34495cb979d0192ee
mohammad asghari <mohammadsgr8@gmail.com>
2023-01-17 22:27:02 -0800

    [UPDATE] : update search result and go to location

--- src/components/map-search-bar/map-search-result.jsx
+++ src/components/map-search-bar/map-search-result.jsx
@@ -1,27 +1,34 @@
 import { List, ThemeIcon } from '@mantine/core';
 import { IconCurrentLocation } from '@tabler/icons';
 import { useMap } from 'react-map-gl';
-import { useSelector } from 'react-redux';
-import { selectSearchResult } from '../../store/mapSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectAction, selectSearchResult, setSearchResult } from '../../store/mapSlice';
 import './map-search-result-style.css';
 const MapSearchResult = () => {
    const { usemap } = useMap();
   const searchResult = useSelector(selectSearchResult);
-    const handleGoLocationCenter = (lng,lat) => {
+  const action = useSelector(selectAction)
+  const dispatch = useDispatch();
+
+
+
+  const handleCLickItem = (lng,lat) => {
+    dispatch(setSearchResult([]))  
     usemap.flyTo({ center: [lat, lng] })
   }
-  
-  
-  
-  
+    
   if(searchResult.length === 0){
     return (
-      <></>
+      <>
+      </>
     )
   }
   return (
     
-    <div style={{width:"100%",height:"100%"}}>
+    <div className='map-search-result-container'
+     style={{width:"96%",
+    height:"15em"
+    }}>
           <List
       spacing="xs"
       size="sm"
@@ -39,14 +46,14 @@ const MapSearchResult = () => {
             <List.Item
              key={item.place_id}
              onClick={()=> {
-              handleGoLocationCenter(item.lat, item.lon)
+              handleCLickItem(item.lat, item.lon)
              }}
              
              >{item.address.amenity}  {item.address.tourism} {item.address.neighbourhood}  {item.address.road}  {item.address.state}  {item.address.suburb}</List.Item>
           )
         })
       }
-     <p style={{paddingTop:"40px"}} ></p>
+     {/* <p style={{paddingTop:"40px"}} ></p> */}
     </List>
     </div>
   )
