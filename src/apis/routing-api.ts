3fa707c2df212dc875354ae705a06fcbd89e1401
mohammad asghari <mohammadsgr8@gmail.com>
2023-02-05 13:35:55 +0200

    [CHORE]: update map api name

--- src/apis/routing-api.ts
+++ src/apis/routing-api.ts
@@ -1,5 +1,5 @@
 
-import http from './configs/mapAPi';
+import http from './configs/mapConfig';
 import OsrmApi from './configs/osrmConfig';
 interface routingEndpointProp {
     routeType: "car" | "bike" | "foot",
