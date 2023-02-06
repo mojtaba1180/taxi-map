bb5c2e38dc676addea491557522ac9e440c26ce1
mojtaba <mohammadsgrdev@gmail.com>
2023-02-06 15:46:36 +0330

    [CHORE,FIX]: refactor

--- src/components/main-map/main-map.jsx
+++ src/components/main-map/main-map.jsx
@@ -4,43 +4,37 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 import qs from "qs";
 import randomColor from 'randomcolor';
 import React, { useEffect, useState } from 'react';
-import { Map, Marker, useMap } from 'react-map-gl';
+import { Map, useMap } from 'react-map-gl';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import env from '../../../env.json';
 import { RoutingApi } from '../../apis/routing-api';
-import { drivers } from '../../json/drivers.json';
 import { mapCenter, selectAction, selectCenter, setActions, setDrag, setLocations, setMarkers } from '../../store/mapSlice';
-import DriverDirections from '../driver-directions';
-import LocationMarker from '../location-marker/location-marker';
-import LayerLineRoute from '../map-direction/layer-line-route';
-import { selectLocations, selectMarkers } from './../../store/mapSlice';
+import { selectLocations } from './../../store/mapSlice';
 import './main-map.css';
 
 
 
-const MainMap = () => {
+const MainMap = ({ children }) => {
     //redux state
     const center = useSelector(selectCenter);
     const { locations, inputIndexSelected } = useSelector(selectLocations);
-    const selectedMarkers = useSelector(selectMarkers);
     const mapStyle = env.VITE_MAP_STYLE;
     const action = useSelector(selectAction);
     const dispatch = useDispatch();
     //react state
-    const [isDrag, setIsDrag] = useState(false);
     const { search } = useLocation();
     const { usemap } = useMap();
+    const [isDrag, setIsDrag] = useState(false);
 
     // ANCHOR handle center mode in search query string 
     const query = qs.parse(search.split("?")[1]);
     const arrayMapCenter = query.center ? query.center.split(",").map(i => Number(i)) : null
 
-    // set actions on map
+
     useEffect(() => {
         dispatch(setDrag(isDrag))
     }, [isDrag]);
-
 
     // ANCHOR Csharp function pass data to Csharp app  
     const Cef = (action, d, z, c) => {
@@ -56,8 +50,6 @@ const MainMap = () => {
             lng: center.lng
         }));
     }
-
-
 
     const handleDragStart = () => {
         setIsDrag(true);
@@ -84,7 +76,6 @@ const MainMap = () => {
             zoom
         });
 
-
         const arr = res.display_name.split(",").reverse();
         let address = "";
         arr.forEach(function (str) {
@@ -106,7 +97,7 @@ const MainMap = () => {
             address: address,
             osm_type: res.osm_type
         });
-        if(action.chooseOnMap){
+        if (action.chooseOnMap) {
             usemap.flyTo({ center: [lng, lat] });
         }
         if (action.isDirection, action.chooseOnMap) {
@@ -134,49 +125,9 @@ const MainMap = () => {
             }
         ]))
     }
-    const handleMarkerDrag = async (event, idx) => {
-        const lng = event.lngLat.lng;
-        const lat = event.lngLat.lat;
-        const zoom = parseInt(center.zoom);
-        const { res, err } = await RoutingApi.getLocation({
-            lat: lat,
-            lon: lng,
-            zoom
-        });
-        if (err) return;
-        let arr = [...locations];
-        const handleUpdateLocation = () => {
-            return arr.map((item, index) => {
-                if (index === idx) {
-                    return {
-                        color: randomColor(),
-                        value: res.display_name,
-                        location: {
-                            ...res,
-                            lat,
-                            lon: lng
-                        }
-                    }
-                } else {
-                    return item
-                }
-            })
-        }
-        dispatch(setLocations(handleUpdateLocation()));
-    }
-
-    const layerStyle = {
-        id: 'point',
-        type: 'circle',
-        paint: {
-            'circle-radius': 10,
-            'circle-color': '#007cbf'
-        }
-    }
 
     return (
         <div className="map-wrap">
-
             <Map
                 mapLib={maplibreGl}
                 initialViewState={{
@@ -196,70 +147,7 @@ const MainMap = () => {
                 onZoomStart={handleDragStart}
                 onZoomEnd={handleDragEnd}
             >
-
-                {/* ############################### Direction & line layer ############################### */}
-
-                {/* Direction line  */}
-                <LayerLineRoute />
-                {/* Driver Directions */}
-                <DriverDirections />
-
-                {/* ############################### Marker ############################### */}
-                
-                {/* center mode marker */}
-                {
-                    center.zoom >= 13 && drivers.map(item => {
-                        return (
-                            <Marker key={item.id} children={
-                                <img src='/car.png' width={15} alt="car" />
-                            }
-                                draggable={!action.isMarkerLocked}
-                                latitude={item.location.latitude}
-                                longitude={item.location.longitude}
-                                rotation={item.location.bearing} />
-                        )
-                    })
-                }
-                {/* direction markers */}
-                {
-                    locations?.map((item, idx) => {
-                        if (item.location.hasOwnProperty("lat") && item.location.hasOwnProperty("lon")) {
-                            return (
-                                <Marker
-                                    key={idx}
-                                    draggable={!action.isMarkerLocked}
-                                    onDragEnd={(e) => handleMarkerDrag(e, idx)}
-                                    children={<LocationMarker title={item.location.display_name} color={item.color} />
-                                    }
-                                    anchor="bottom"
-                                    // onDrag={(e) => console.log(e)}
-                                    latitude={item.location?.lat} longitude={item.location?.lon} />
-                            )
-                        }
-                    })
-                }
-                {/* choose on map markers */}
-                {
-                    (selectedMarkers.length > 0 && action.chooseOnMap) && selectedMarkers?.map((item, idx) => {
-                        if (item.location.hasOwnProperty("lat") && item.location.hasOwnProperty("lon")) {
-                            return (
-                                <Marker
-                                    key={idx}
-                                    draggable={!action.isMarkerLocked}
-                                    children={<LocationMarker title={item.location.display_name} />
-                                    }
-                                    anchor="bottom"
-                                    // onDrag={(e) => console.log(e)}
-                                    latitude={item.location?.lat} longitude={item.location?.lon} />
-                            )
-                        }
-                    })
-                }
-                {/* Default location marker */}
-                {(!action.isDirection || action.isSetLocationByMarker) && <LocationMarker centerMode={true} isDrag={isDrag} />}
-                
-                
-                
+                {children}
             </Map>
 
         </div>
