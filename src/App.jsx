b347f5d8367b4c4291d41ab8c826725b3d70b25f
mojtaba <mohammadsgrdev@gmail.com>
2023-02-10 13:17:36 +0330

    [UPDATE]: added dynamic app name

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { Helmet } from "react-helmet";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
 const baseUrl = import.meta.env.BASE_URL;
 function App() {
   const router = createBrowserRouter(routes, { basename: import.meta.env.BASE_URL ? import.meta.env.BASE_URL : "/" })
   //handle route query
-  // useLayoutEffect(() => {
-  //  MapQuery()
-  // })
 
-  return (<RouterProvider router={router} />)
+  return (
+    <>
+      <Helmet>
+        <title>{import.meta.env.VITE_APP_NAME}</title>
+      </Helmet>
+      <RouterProvider router={router} />
+    </>
+  )
 }
 
 export default App
