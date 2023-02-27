d69439af05fbce47a8315e4fae53d5c42d0c5eac
mojtaba <mohammadsgrdev@gmail.com>
2023-02-27 18:18:12 +0330

    [UPDATE] : fixed bug

--- src/helpers/handleNumToBol.ts
+++ src/helpers/handleNumToBol.ts
@@ -2,9 +2,9 @@
 
 export function NumToBol(value: 0 | 1 | boolean) {
     if (Number(value) === 1) {
-        return false
-    } else if (Number(value) === 0) {
         return true
+    } else if (Number(value) === 0) {
+        return false
     } else {
         return value
     }
